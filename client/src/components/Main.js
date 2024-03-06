5e326a70d0644ea1a964372921f6d3ad4cd79e93
Kolisang <kolisang9@gmail.com>
2024-03-06 17:38:50 +0200

    add navbar & main component

---
+++ client/src/components/Main.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+function Main(){
+  return (
+    <div>
+        <div className="container">
+            <div className="headline-text">
+                <h1>Main article heading</h1>
+                <h5>Description</h5>
+                <button>Read more</button>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Main
---
+++ client/src/components/Navbar.js
@@ -0,0 +1,17 @@
+
+
+function Navbar(){
+    return (
+        <nav>
+            <h3>TechoThoughts</h3>
+            <ul>
+                <li><a href="#">Tech</a> /</li>
+                <li><a href="#">Reviews</a> /</li>
+                <li><a href="#">Science</a> /</li>
+                <li><a href="#">Entertainment</a> </li>
+            </ul>
+        </nav>
+    );
+}
+
+export default Navbar
