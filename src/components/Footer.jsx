51b75d0b3f36d8186bd2f1314b1dcc8ec8dbd81b
Nicolette Steenkamp <steenkampnicolette815@gmail.com>
2024-03-16 17:20:50 +0200

    edited

---
+++ src/components/Footer.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import Logo from "../images/logo.png";
+
+const Footer = () => {
+  return (
+    <footer>
+      <img src={Logo} alt="logo" />
+      <span>
+        Made by <b>Koli , Nicolette & Asiphile</b>
+      </span>
+    </footer>
+  );
+};
+
+export default Footer;
