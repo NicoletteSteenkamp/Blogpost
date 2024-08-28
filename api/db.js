c1e2f81f5c12cfa418d77e74e6fcb9baf2ac7feb
Nicolette Steenkamp <steenkampnicolette815@gmail.com>
2024-08-28 11:04:53 +0200

    Update db.js

--- api/db.js
+++ api/db.js
@@ -3,27 +3,25 @@ import dotenv from 'dotenv';
 
 dotenv.config();
 
-export const db = mysql.createConnection({
+const db = mysql.createConnection({
   host: process.env.AIVEN_HOST,
   user: process.env.AIVEN_USER,
   password: process.env.AIVEN_PASSWORD,
   database: process.env.AIVEN_DATABASE,
   port: process.env.AIVEN_PORT,
-  connectTimeout: 60000,
   ssl: { rejectUnauthorized: false }  // Aiven typically requires SSL connections
 });
 
-db.connect((err) => {
-  if (err) {
-    console.error('Error connecting to the Aiven database:', err.stack);
-    return;
-  }
-  console.log('Connected to the Aiven MySQL database.');
-});
-
-
-// Ping the database every 2 days
-setInterval(pingDatabase, 1000 * 60 * 60 * 24 * 2);
+// Define the pingDatabase function
+function pingDatabase() {
+  db.query('SELECT 1', (err) => {
+    if (err) {
+      console.error('Error pinging the database:', err.stack);
+    } else {
+      console.log('Database connection is active.');
+    }
+  });
+}
 
 // Connect to the database initially
 db.connect((err) => {
@@ -35,4 +33,9 @@ db.connect((err) => {
   
   // Initial ping
   pingDatabase();
+  
+  // Set up interval to ping the database every 2 days
+  setInterval(pingDatabase, 1000 * 60 * 60 * 24 * 2);
 });
+
+
