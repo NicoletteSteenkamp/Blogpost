b92f9f10471ee0507268ee3141c8cddd74aae9f1
Nicolette Steenkamp <steenkampnicolette815@gmail.com>
2024-08-28 10:04:32 +0200

    hosted schema

--- api/db.js
+++ api/db.js
@@ -4,19 +4,19 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 export const db = mysql.createConnection({
-  host: 'localhost',
-  user: 'root',
-  password: process.env.DB_KEY,
-  database: 'blogpost',
+  host: process.env.AIVEN_HOST,
+  user: process.env.AIVEN_USER,
+  password: process.env.AIVEN_PASSWORD,
+  database: process.env.AIVEN_DATABASE,
+  port: process.env.AIVEN_PORT,
   connectTimeout: 60000,
+  ssl: { rejectUnauthorized: false }  // Aiven typically requires SSL connections
 });
-
-
 
 db.connect((err) => {
   if (err) {
-    console.error('Error connecting to the database:', err.stack);
+    console.error('Error connecting to the Aiven database:', err.stack);
     return;
   }
-  console.log('Connected to the MySQL database.');
+  console.log('Connected to the Aiven MySQL database.');
 });
